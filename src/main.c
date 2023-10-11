6d671007dc42c21c0d1b713807ffe10b9e7f2057
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-11 14:58:13 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
-#include <stddef.h>
-#include <sys/syscall.h>
-
 int main() {
   const char message[] = "Hello, World!\n";
-  const size_t message_length = sizeof(message) - 1;
-  
-  syscall(4, 1, message, message_length);
-  
+  const unsigned long message_length = sizeof(message) - 1;
+
+  asm volatile (
+    "mov $1, %%rax\n"         // Syscall number for write
+    "mov $1, %%rdi\n"         // File descriptor for stdout
+    "mov %0, %%rsi\n"         // Pointer to message
+    "mov %1, %%rdx\n"         // Message length
+    "syscall\n"               // Invoke the syscall
+    :
+    : "r" (message), "r" (message_length)
+    : "%rax", "%rdi", "%rsi", "%rdx"
+  );
+
   return 0;
 }
